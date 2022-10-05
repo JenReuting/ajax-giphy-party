6e886ebb2ef751496011f50e6a0cb8f3c37cf179
Gordon Ball <gball261@gmail.com>
2022-10-04 20:13:39 -0400

    Gif works

--- app.js
+++ app.js
@@ -5,17 +5,30 @@ const gifList = document.querySelector(".gif-list");
 
 /** Gets data from the Giphy API and prints it to the console */
 async function getGif () {
-  let response = await axios.get("http://api.giphy.com/v1/gifs/search?q=hilarious&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym&limit=1")
-  .then(appendGifToPage);
-  console.log("data returned in function: ", response.data);
-  return response.data;
+  // let response = await axios.get("http://api.giphy.com/v1/gifs/search?q=hilarious&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym&limit=1")
+  // .then(appendGifToPage);
+
+  // return response.data;
+
+  const response = await axios.get("http://api.giphy.com/v1/gifs/search?q=hilarious&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym&limit=1");
+  appendGifToPage(response.data.data[0].images.original.url)
+  console.log(response);
+  // console.log("data returned in function: ", response.data);
 }
+
+
+// const newGif = JSON.parse(getGif());
+// console.log(newGif);
 
 function appendGifToPage(elem) {
   let newGif = document.createElement("div");
-  newGif.innerHTML = `<p>This function works! ${elem}</p>`
+  newGif.innerHTML = `<img src="${elem}">`
   gifList.append(newGif);
 }
+
+
+
+
 
 
 button.addEventListener("click", getGif);
